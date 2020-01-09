14afd6fd9c12a2a122a2f1513dbb2333c4e26fbf
boccaccioo <marionboccaccio@gmail.com>
2020-01-09 11:10:13 +0100

    add some css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
+// import axios from "axios";
 import "./App.css";
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
+import AllTeeShirtz from "./components/AllTeeShirtz";
 
 function App() {
   return (
     <div>
       <Header />
       <Sidebar />
-
+      <AllTeeShirtz />
       <Footer />
     </div>
   );
---
+++ src/components/AllTeeShirtz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllTeeShirtz() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AllTeeShirtz
