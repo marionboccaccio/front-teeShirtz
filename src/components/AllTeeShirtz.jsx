a822890cfe36e522b5ab2447bd6d2a5fc69206c5
boccaccioo <marionboccaccio@gmail.com>
2020-01-13 14:37:11 +0100

    work on the map

--- src/components/AllTeeShirtz.jsx
+++ src/components/AllTeeShirtz.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function AllTeeShirtz() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [teeShirtz, setTeeShirtz] = useState([]);
 
-export default AllTeeShirtz
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_BACKEND_URL + "/teeShirtz")
+      .then(dbRes => {
+        setTeeShirtz(dbRes.data);
+        console.log("coucou");
+      })
+      .catch(dbErr => console.error(dbErr));
+  }, []);
+
+  return (
+    <div className="teeShirt">
+      {teeShirtz.map((tshirt, i) => (
+        <>
+          <div className="title">
+            <h4>{tshirt.name}</h4>
+            <h6>{tshirt.brand}</h6>
+          </div>
+          <img src={tshirt.image} alt="img" />
+          <p>{tshirt.price} € </p>
+        </>
+      ))}
+    </div>
+  );
+}
+export default AllTeeShirtz;
